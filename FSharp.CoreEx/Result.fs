df01be8bb5ba3fd7303ad1192be57eb0353b7056
ltaylor <ltaylor@travelrepublic.co.uk>
2016-12-07 23:14:24 +0000

    Result to module

--- FSharp.CoreEx/Result.fs
+++ FSharp.CoreEx/Result.fs
@@ -1,52 +1,51 @@
-module Result.Core
+module Result
 
-  type Result<'a, 'b> = 
+  type T<'a, 'b> = 
     Ok of 'a
     | Error of 'b
 
-  type Result() =
-    static member ofOption error option = 
-      match option with
-      | None -> Error error
-      | Some o -> Ok o
+  let ofOption error option = 
+    match option with
+    | None -> Error error
+    | Some o -> Ok o
 
-    static member throwIfError result = 
-      match result with
-      | Ok a -> a
-      | Error error -> failwithf "Result.Error: %A" error
+  let throwIfError result = 
+    match result with
+    | Ok a -> a
+    | Error error -> failwithf "Result.Error: %A" error
 
-    static member ret a = Ok a
+  let ret a = Ok a
 
-    static member tryGetOk result = 
-      match result with
-      | Ok ok -> Some ok
-      | Error _ -> None
+  let tryGetOk result = 
+    match result with
+    | Ok ok -> Some ok
+    | Error _ -> None
 
-    static member ok (r : Result<'a, _>) = (Result.tryGetOk >> Option.get) r
+  let ok (r : T<'a, _>) = (tryGetOk >> Option.get) r
 
-    static member apply (resultF : Result<'a ->'b, _>) (resultA : Result<'a, _>) = 
-      match resultF, resultA with
-      | Ok f, Ok a -> Ok (f a)
-      | Error fError, _ -> Error fError
-      | _ , Error aError -> Error aError
+  let apply (resultF : T<'a ->'b, _>) (resultA : T<'a, _>) = 
+    match resultF, resultA with
+    | Ok f, Ok a -> Ok (f a)
+    | Error fError, _ -> Error fError
+    | _ , Error aError -> Error aError
 
-    static member map (fOk, fError) result = 
-      match result with
-      | Ok a  -> Ok (fOk a)
-      | Error error -> Error(fError error)
+  let map (fOk, fError) result = 
+    match result with
+    | Ok a  -> Ok (fOk a)
+    | Error error -> Error(fError error)
 
-    static member mapOk f = Result.map (f, id)
-    static member mapError f = Result.map (id, f)
+  let mapOk f = map (f, id)
+  let mapError f = map (id, f)
 
-    static member bindOk (f : 'a -> Result<'b, 'c>) aR = 
-      match aR with
-      | Ok a -> a |> f
-      | Error x -> Error x
+  let bindOk (f : 'a -> T<'b, 'c>) aR = 
+    match aR with
+    | Ok a -> a |> f
+    | Error x -> Error x
 
-    static member ofAsyncToAsync xRA = 
-      match xRA with
-      | Ok xA -> 
-        async { 
-          let! x = xA 
-          return Ok x}
-      | Error e -> async { return Error e}
+  let ofAsyncToAsync xRA = 
+    match xRA with
+    | Ok xA -> 
+      async { 
+        let! x = xA 
+        return Ok x}
+    | Error e -> async { return Error e}
